9756b40aca39d452f522ae604dc1b40ca82d893a
Zhanna Doronina <doronina.jane@mail.ru>
2023-06-06 22:38:17 +0100

    feat: completed the CardDeal component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({styles}) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles}`}>
       Get started
     </button>
   )
